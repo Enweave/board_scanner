483a4899ccb90560387564a62c1852e796c88602
Insy Weaver <enweave@gmail.com>
2023-06-25 02:09:19 +0500

    storable settings

--- modules/app.py
+++ modules/app.py
@@ -1,3 +1,4 @@
+import json
 import os
 import typing
 
@@ -17,14 +18,14 @@ class ScannerApp:
         fps_limit: int = 30,
         webcam_index: int = 0,
         still_image: typing.Union[str, os.PathLike] = None,
+        use_default_session_settings: bool = False,
     ):
         self._window_name = window_name
         self._fps_limit = fps_limit
-        self._img_filter_params = {
-            BRIGHTNESS_KEY: 127,
-            CONTRAST_KEY: 127,
-            BW_KEY: 0,
-        }
+        self._use_default_session_settings = use_default_session_settings
+        self.settings_path = f"{window_name}_settings.json"
+
+        self.SESSION_SETTINGS = self._load_session_settings()
         self._webcam_index = webcam_index
         self._still_image = still_image
 
@@ -34,6 +35,24 @@ class ScannerApp:
         self._setup_window()
         self._setup_trackbars()
 
+    def _load_session_settings(self) -> dict:
+        if (
+            os.path.isfile(self.settings_path)
+            and not self._use_default_session_settings
+        ):
+            with open(self.settings_path, "r") as f:
+                return json.load(f)
+        else:
+            return {
+                BRIGHTNESS_KEY: 127,
+                CONTRAST_KEY: 127,
+                BW_KEY: 0,
+            }
+
+    def _save_session_settings(self):
+        with open(self.settings_path, "w") as f:
+            json.dump(self.SESSION_SETTINGS, f)
+
     def _cycle(self):
         frame = self._camera.read()
         frame = self._apply_filters(frame)
@@ -41,6 +60,7 @@ class ScannerApp:
 
     def _stop(self):
         self._camera.__del__()
+        self._save_session_settings()
         cv2.destroyAllWindows()
 
     def run(self):
@@ -55,26 +75,22 @@ class ScannerApp:
         cv2.namedWindow(self._window_name)
 
     def _setup_trackbars(self):
-        mk_trakbar(self._window_name, self._img_filter_params, BRIGHTNESS_KEY, 255)
-        mk_trakbar(self._window_name, self._img_filter_params, CONTRAST_KEY, 255)
-        mk_trakbar(self._window_name, self._img_filter_params, BW_KEY, 1)
+        mk_trakbar(self._window_name, self.SESSION_SETTINGS, BRIGHTNESS_KEY, 255)
+        mk_trakbar(self._window_name, self.SESSION_SETTINGS, CONTRAST_KEY, 255)
+        mk_trakbar(self._window_name, self.SESSION_SETTINGS, BW_KEY, 1)
 
     def _apply_filters(self, img: np.ndarray) -> np.ndarray:
         img = apply_brightness_contrast(
             img,
-            self._img_filter_params.get(BRIGHTNESS_KEY),
-            self._img_filter_params.get(CONTRAST_KEY),
+            self.SESSION_SETTINGS.get(BRIGHTNESS_KEY),
+            self.SESSION_SETTINGS.get(CONTRAST_KEY),
         )
-        if self._img_filter_params.get(BW_KEY):
+        if self.SESSION_SETTINGS.get(BW_KEY):
             img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         return img
 
     def _setup_camera(self) -> Droidcam:
         if self._still_image:
-            return Droidcam(
-                use_webcam=False, img_src=self._still_image
-            )
+            return Droidcam(use_webcam=False, img_src=self._still_image)
         else:
-            return Droidcam(
-                use_webcam=True, webcam_index=self._webcam_index
-            )
+            return Droidcam(use_webcam=True, webcam_index=self._webcam_index)
