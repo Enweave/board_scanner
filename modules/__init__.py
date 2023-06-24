2aa5dfbf1674651e061de473ca8b721a39859baf
Insy Weaver <enweave@gmail.com>
2023-06-25 03:07:02 +0500

    roi control

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,3 +1,7 @@
+import numpy as np
 BRIGHTNESS_KEY = "brightness"
 CONTRAST_KEY = "contrast"
 BW_KEY = "bw"
+ROI_WIDTH = "roi_width"
+ROI_HEIGHT = "roi_height"
+HALFPI = np.pi / 2
--- modules/app.py
+++ modules/app.py
@@ -4,9 +4,9 @@ import typing
 
 from fps_limiter import LimitFPS
 
-from modules import BRIGHTNESS_KEY, CONTRAST_KEY, BW_KEY
+from modules import BRIGHTNESS_KEY, CONTRAST_KEY, BW_KEY, ROI_WIDTH, ROI_HEIGHT, HALFPI
 from modules.camera import Droidcam
-from modules.utils import apply_brightness_contrast, mk_trakbar
+from modules.utils import apply_brightness_contrast, mk_trakbar, drawAxis
 import numpy as np
 import cv2
 
@@ -36,26 +36,50 @@ class ScannerApp:
         self._setup_trackbars()
 
     def _load_session_settings(self) -> dict:
+        default_settings = {
+                BRIGHTNESS_KEY: 127,
+                CONTRAST_KEY: 127,
+                BW_KEY: 0,
+                ROI_WIDTH: 100,
+                ROI_HEIGHT: 100,
+            }
         if (
             os.path.isfile(self.settings_path)
             and not self._use_default_session_settings
         ):
             with open(self.settings_path, "r") as f:
-                return json.load(f)
+                loaded_settings = json.load(f)
+                for key in default_settings.keys():
+                    if key not in loaded_settings.keys():
+                        loaded_settings[key] = default_settings[key]
+                return loaded_settings
         else:
-            return {
-                BRIGHTNESS_KEY: 127,
-                CONTRAST_KEY: 127,
-                BW_KEY: 0,
-            }
+            return default_settings
 
     def _save_session_settings(self):
         with open(self.settings_path, "w") as f:
             json.dump(self.SESSION_SETTINGS, f)
 
+    def _draw_roi(self, img: np.ndarray) -> np.ndarray:
+        roi_width = self.SESSION_SETTINGS.get(ROI_WIDTH)
+        roi_height = self.SESSION_SETTINGS.get(ROI_HEIGHT)
+        img_height, img_width = img.shape[:2]
+        center = (img_width // 2, img_height // 2)
+        x = (img_width - roi_width) // 2
+        y = (img_height - roi_height) // 2
+        cv2.rectangle(
+            img, (x, y), (x + roi_width, y + roi_height), (0, 255, 0), thickness=4
+        )
+        # draw crosshair
+        drawAxis(img, center, (0, 0, 255), 0)
+        drawAxis(img, center, (0, 0, 255), HALFPI)
+        # drawAxis(img, (img_height // 2, img_width // 2), (0, 0, 255), 0.5)
+        return img
+
     def _cycle(self):
         frame = self._camera.read()
         frame = self._apply_filters(frame)
+        frame = self._draw_roi(frame)
         cv2.imshow(self._window_name, frame)
 
     def _stop(self):
@@ -78,6 +102,8 @@ class ScannerApp:
         mk_trakbar(self._window_name, self.SESSION_SETTINGS, BRIGHTNESS_KEY, 255)
         mk_trakbar(self._window_name, self.SESSION_SETTINGS, CONTRAST_KEY, 255)
         mk_trakbar(self._window_name, self.SESSION_SETTINGS, BW_KEY, 1)
+        mk_trakbar(self._window_name, self.SESSION_SETTINGS, ROI_WIDTH, 700)
+        mk_trakbar(self._window_name, self.SESSION_SETTINGS, ROI_HEIGHT, 700)
 
     def _apply_filters(self, img: np.ndarray) -> np.ndarray:
         img = apply_brightness_contrast(
@@ -87,6 +113,7 @@ class ScannerApp:
         )
         if self.SESSION_SETTINGS.get(BW_KEY):
             img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
         return img
 
     def _setup_camera(self) -> Droidcam:
